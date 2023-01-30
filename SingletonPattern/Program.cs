5317f7401b594912291e8d2103c7f7dadd003c9d
quyentho <thokimquangquyen@gmail.com>
2023-01-30 18:34:31 +0700

    singleton pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using SingletonPattern;
+
+var singleton = Singleton.GetInstance();
+
+var singleton2 = Singleton.GetInstance();
+
+//if (singleton == singleton2)
+//{
+//    Console.WriteLine("Same instance");
+//}
+//else
+//{
+//    Console.WriteLine("Not the same");
+//}
+
+
+// thread safe test
+
+var tasks = new Task[100];
+for (int i = 0; i < tasks.Length; i++)
+{
+    tasks[i] = Task.Run(() => Singleton.GetInstance());
+}
+
+//
+await Task.WhenAll(tasks);
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private static readonly object _lock = new object();
+        private Singleton()
+        {
+            Console.WriteLine("Im a singleton");
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (_instance is null)
+            {
+                // in multi threaded app, there maybe multiple thread go to this line at the same time,
+                // then it should wait for only one thread to obtain the lock.
+                lock (_lock)
+                {
+                    // recheck because after the lock is release,
+                    // we want to make sure only the first
+                    // thread will be able to new up an instance.
+                    if (_instance == null)
+                    {
+                        Console.WriteLine("Instance is null, create an instance");
+                        _instance = new Singleton();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+    }
+}
